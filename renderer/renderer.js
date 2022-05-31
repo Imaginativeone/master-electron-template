0cdb992af775b4b77d93bf9e3e661fc415efd057
Doug Franklin <doug@dougfranklin.com>
2022-05-31 19:22:26 -0400

    Renderer filled out a bit

--- renderer/renderer.js
+++ renderer/renderer.js
@@ -1,3 +1,18 @@
 // This file is required by the index.html file and will
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
+
+const linksSection       = document.querySelector('.links');
+const errorMessage       = document.querySelector('.error-message');
+const newLinkForm        = document.querySelector('.new-link-form');
+const newLinkUrl         = document.querySelector('.new-link-url');
+const newLinkSubmit      = document.querySelector('.new-link-submit');
+const clearStorageButton = document.querySelector('.clear-storage');
+
+newLinkUrl.addEventListener('keyup', () => {
+  newLinkSubmit.disabled = !newLinkUrl.validity.valid;
+});
+
+const clearForm = () => {
+  newLinkUrl.value = null;
+};
