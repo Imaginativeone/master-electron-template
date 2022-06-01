1abb391c82fd072f45505eb9c784cf6d1d940092
Doug Franklin <doug@dougfranklin.com>
2022-05-31 21:02:37 -0400

    FP functions for parsing and extracting title

--- renderer/renderer.js
+++ renderer/renderer.js
@@ -1,6 +1,9 @@
 // This file is required by the index.html file and will be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
 
+// Chromium provides a parser for the returned DOM data.
+const parser = new DOMParser();
+
 const linksSection       = document.querySelector('.links');
 const errorMessage       = document.querySelector('.error-message');
 const newLinkForm        = document.querySelector('.new-link-form');
@@ -17,25 +20,25 @@ const clearForm = () => {
 };
 
 newLinkForm.addEventListener('submit', (event) => {
-
   event.preventDefault();
 
   const url = newLinkUrl.value; // From the input control...
 
-  // More code to come...
-  console.log('url', url);
-
   fetch(url)
-    .then((response) => {
-      console.log('response', response);
-      // console.log('response.text()', response.text());
-      return response.text();
-    })
-    .then((info) => {
-
-      console.log('info', info);
-      console.log('url', url); // From line 23
-
-    })
-
+    .then(response => response.text())
+    .then(parseResponse)
+    .then(findTitle)
 });
+
+// Helper functions for zero-ing in on the <title> element
+// See the definition of a "calculation" in the book, "Grokking Simplicity" by Eric Normand - 
+// a book about Functional Programming (FP). Normand also has an interesting podcast.
+const parseResponse = (text) => {
+  // console.log(parser.parseFromString(text, 'text/html'));
+  return parser.parseFromString(text, 'text/html');
+}
+
+const findTitle = (nodes) => {
+  // console.log(nodes.querySelector('title').innerText);
+  return nodes.querySelector('title').innerText;
+}
